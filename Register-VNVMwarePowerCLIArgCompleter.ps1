181c7b25269fecd454fb0dc01bdc1bc3ccd53f04
MTBoren <technology@ddayinc.com>
2019-08-04 19:26:56 -0400

    add completer for -Server for ~350 cmdlets, fix name completer for VM, Template

--- Register-VNVMwarePowerCLIArgCompleter.ps1
+++ Register-VNVMwarePowerCLIArgCompleter.ps1
@@ -59,7 +59,9 @@ process {
     ## VM or template name completer
     $sbGetVmOrTemplateNameCompleter = {
         param($commandName, $parameterName, $wordToComplete, $commandAst, $fakeBoundParameter)
-        Get-View -ViewType VirtualMachine -Property Name, Runtime.Powerstate -Filter @{Name = "^${wordToComplete}"; "Config.Template" = ($commandName -ne "Get-VM" -and $parameterName -ne "VM").ToString()} | Sort-Object -Property Name | Foreach-Object {
+        ## make the regex pattern to use for Name filtering for given View object (convert from globbing wildcard to regex pattern, to support globbing wildcard as input)
+        $strNameRegex = if ($wordToComplete -match "\*") {$wordToComplete.Replace("*", ".*")} else {$wordToComplete}
+        Get-View -ViewType VirtualMachine -Property Name, Runtime.Powerstate -Filter @{Name = "^${strNameRegex}"; "Config.Template" = ($commandName -ne "Get-VM" -and $parameterName -ne "VM").ToString()} | Where-Object {$fakeBoundParameter.$parameterName -notcontains $_.Name} | Sort-Object -Property Name | Foreach-Object {
             New-Object -TypeName System.Management.Automation.CompletionResult -ArgumentList (
                 $_.Name,    # CompletionText
                 $_.Name,    # ListItemText
@@ -198,6 +200,25 @@ process {
     } ## end scriptblock
 
     Register-ArgumentCompleter -CommandName New-VM, Set-VM -ParameterName GuestID -ScriptBlock $sbGuestIDEnumNameCompleter
+
+
+
+    ## Name completer for -Server param for cmdlets that are dealing with vCenter/VIServer (not for cmdlets where -Server is something else, like cmdlets for vROps, NSXT, HorizonView, VMC, etc.)
+    $sbVIServerNameCompleter = {
+        param($commandName, $parameterName, $wordToComplete, $commandAst, $fakeBoundParameter)
+        $global:DefaultVIServers | Where-Object {$_.Name -like "${wordToComplete}*"} | Where-Object {$fakeBoundParameter.$parameterName -notcontains $_.Name} | Sort-Object -Property Name | Foreach-Object {
+            ## make the Completion and ListItem text values; happen to be the same for now, but could be <anything of interest/value>
+            New-Object -TypeName System.Management.Automation.CompletionResult -ArgumentList (
+                $_.Name,    # CompletionText
+                $_.Name,    # ListItemText
+                [System.Management.Automation.CompletionResultType]::ParameterValue,    # ResultType
+                ("{0} ({1})" -f $_.Name, $_.User)    # ToolTip
+            )
+        } ## end foreach-object
+    } ## end scriptblock
+
+    Register-ArgumentCompleter -CommandName (Get-Command -Module (Write-Output VMware.VimAutomation.Core, VMware.VimAutomation.License, VMware.VimAutomation.Security, VMware.VimAutomation.Srm, VMware.VimAutomation.Storage, VMware.VimAutomation.StorageUtility, VMware.VimAutomation.Vds, VMware.VumAutomation) -ParameterName Server) -ParameterName Server -ScriptBlock $sbVIServerNameCompleter
+
 
 
     ## will need more research (are specific to a particular instance of an object, for example, or current retrieval method is sllloowww)
